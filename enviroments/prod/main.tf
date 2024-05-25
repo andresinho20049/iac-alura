5c06e5138d38e8c92d6e688142856e9c637ada90
andresinho20049 <andre.andresinho2009@hotmail.com>
2024-05-25 16:50:57 -0300

    use branch team

--- enviroments/prod/main.tf
+++ enviroments/prod/main.tf
@@ -1,9 +1,9 @@
-module "aws-dev" {
+module "aws-prd" {
   source = "../../infla"
   instance_t = "t2.micro"
   enviroment_team = "production"
 }
 
 output "IP_PROD" {
-  value = module.aws-dev.IP_Public
+  value = module.aws-prd.IP_Public
 }
